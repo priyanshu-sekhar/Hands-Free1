7241804a7a0a0e0617cc6a4cb87735d69d4e521d
priyanshu-sekhar <pri.patra@gmail.com>
2015-01-13 15:55:42 +0530

    Changes

--- app/src/main/java/com/example/android/handsfree/LaunchTutorial.java
+++ app/src/main/java/com/example/android/handsfree/LaunchTutorial.java
@@ -281,22 +281,6 @@ public class LaunchTutorial extends FragmentActivity{
                         }
                     });
 
-            // Demonstration of navigating to external activities.
-//            rootView.findViewById(R.id.demo_external_activity)
-//                    .setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View view) {
-//                            // Create an intent that asks the user to pick a photo, but using
-//                            // FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET, ensures that relaunching
-//                            // the application from the device home screen does not return
-//                            // to the external activity.
-//                            Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
-//                            externalActivityIntent.setType("image/*");
-//                            externalActivityIntent.addFlags(
-//                                    Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-//                            startActivity(externalActivityIntent);
-//                        }
-//                    });
 
             return rootView;
         }
